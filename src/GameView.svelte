b7ca78ba339dbdd14521567bb3bf3b948750d8f7
puffio <>
2022-06-22 16:12:21 +0000

    Moved some functions that didn't need to be in .js files

--- src/GameView.svelte
+++ src/GameView.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { assets, render, update, keys, input } from "./game.js";
+    import { assets, keys } from "./game.js";
 	import { onMount } from 'svelte';
     import { getContext } from 'svelte';
 
@@ -12,7 +12,7 @@
         }
         if (packet) {
             // console.log(packet);
-            world = update(world, packet);
+            update(packet);
         }
         // timer.tick();
         // console.log(timer.fps);
@@ -33,7 +33,7 @@
 
     function frame() {
         try {
-            render($assets, world, ctx, width, height);
+            render();
             animationFrame = requestAnimationFrame(frame);
         } catch(err) {
             throw err;
@@ -48,6 +48,108 @@
 			cancelAnimationFrame(animationFrame);
 		};
     });
+
+    function zip(arrays) { // Modified from https://stackoverflow.com/questions/4856717/javascript-equivalent-of-pythons-zip-function
+        return arrays[0].map(
+            (_, i) => arrays.map(array => array[i])
+        );
+    }
+    
+    function update(packet) {
+        // Maybe add entity & tile data?
+        let [tile_xs, tile_ys, tile_types, entity_xs, entity_ys, entity_ids, player_index] = packet;
+    
+        world["player_x"] = entity_xs[player_index];
+        world["player_y"] = entity_ys[player_index];
+    
+        for (let [tile_x, tile_y, tile_type] of zip([tile_xs, tile_ys, tile_types])) {
+            world["tilemap"][`(${tile_x}, ${tile_y})`] = {
+                x: tile_x,
+                y: tile_y,
+                type: tile_type
+            };
+        }
+    
+        world["entities"] = [];
+        for (let [entity_x, entity_y, entity_id] of zip([entity_xs, entity_ys, entity_ids])) {
+            world["entities"].push({
+                x: entity_x,
+                y: entity_y,
+                id: entity_id
+            });
+        }
+    }
+    
+    function render() {
+        ctx.fillStyle = "#3fd7e8";
+        ctx.fillRect(0, 0, width, height);
+    
+        let veiwHeight = 7;
+        let cameraX = world["player_x"];
+        let cameraY = world["player_y"];
+    
+        let will_render = (screenSize, x, y) => {
+            return !(x + screenSize < 0 || x > width
+               || y + screenSize < 0 || y > height); 
+        }
+        
+        for (let tile of Object.values(world.tilemap)) {
+            if (tile.type != 0) {
+                let name = {
+                    1: "grass"
+                }[tile.type];
+                let image = $assets[name + ".png"];
+                let size = 1
+                let scale = height / veiwHeight;
+                let x = (tile["x"] - cameraX) * scale + width / 2;
+                let y = -(tile["y"] - cameraY) * scale + height / 2;
+                x -= scale / 2;
+                y -= scale / 2;
+                let screenSize = scale * size;
+                if (!will_render(screenSize, x, y)) {
+                    continue;
+                }
+                ctx.drawImage(
+                    image,
+                    x,
+                    y,
+                    screenSize,
+                    screenSize
+                );
+            }
+        }
+    
+        for (let entity of world.entities) {
+            // console.log(entity.id + 126);
+            // console.log(Math.floor((entity.id + 126) / 16))
+            let name = {
+                1: "puff"
+            }[Math.floor((entity.id + 126) / 16)];
+            let image = $assets[name + ".png"];
+            let size = 1
+            let scale = height / veiwHeight;
+            let x = (entity.x - cameraX) * scale + width / 2;
+            let y = -(entity.y - cameraY) * scale + height / 2;
+            x -= scale / 2;
+            y -= scale / 2;
+            ctx.drawImage(
+                image,
+                x,
+                y,
+                scale * size,
+                scale * size
+            );
+        }
+    }
+
+    function input(keySet) {
+        let keys = Array.from(keySet);
+        let keyBytes = new Uint8Array(keys.length);
+        for (let key in keys) {
+            keyBytes[key] = keys[key];
+        }
+        return keyBytes.buffer;
+    }
 </script>
 
 <article>
