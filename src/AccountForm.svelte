81e2c4a5ac72c77914188d27fe86747b885e7efa
puffio <51675874+budopod1@users.noreply.github.com>
2022-08-25 17:40:38 +0000

    Added commas

--- src/AccountForm.svelte
+++ src/AccountForm.svelte
@@ -26,7 +26,7 @@
         <div class="input">
             <label for="username">Username</label>
             <input id="username" bind:value={username} minlength="3" maxlength="20" required pattern="\w*">
-            <small class="info">Username can only have letters numbers and underscores</small>
+            <small class="info">Username can only have letters, numbers, and underscores</small>
         </div>
         <div class="input">
             <label for="password">Password</label>
